a11de7501e6b022af003917074507097143ff3dd
lrosa007 <lucas@consys.io>
2017-01-06 09:36:39 -0500

    bring in deps maru for routes and earmark for markdown

--- mix.exs
+++ mix.exs
@@ -17,16 +17,8 @@ defmodule Quine.Mixfile do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:maru, "~> 0.11"},
+     {:earmark, "~> 1.0"}]
   end
 end

081ee90fc9bc64f2ff33d06b02555082fa4a0b78
lrosa007 <lucas@consys.io>
2017-01-06 13:19:42 -0500

    javascript for homepage is done, missing syntax highlighting

--- lib/quine.ex
+++ lib/quine.ex
@@ -7,11 +7,15 @@ defmodule Quine do
     parsers: [:urlencoded, :json, :multipart]
 
   get "/code" do
-    json conn, %{code: File.read! "lib/quine.ex" }
+    conn
+    |> put_status(200)
+    |> json(%{code: File.read! "lib/quine.ex" })
   end
 
   get "/home" do
-    html conn, home_page
+    conn
+    |> put_status(200)
+    |> html(home_page)
   end
 
   rescue_from :all do
@@ -24,6 +28,7 @@ defmodule Quine do
     """
     <html>
       <head>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/fetch/2.0.1/fetch.min.js"></script>
       </head>
       <body>
         <h1>Maru Quine</h1>
@@ -33,20 +38,55 @@ defmodule Quine do
         <h2>Intro</h2>
 
         <p>Hello, I am a server hosted on heroku. The micro-framework used in my creation was <a href="https://github.com/elixir-maru/maru)">Maru</a>.
-        This mini app only has two routes <code>/code</code> and <code>/home</code>. The first route simply returns
-        some JSON containing the source code for this app and the second route displays this page
-        you are looking at right now.</p>
+        I only have two routes <code>/code</code> and <code>/home</code>. The first route simply returns
+        some JSON containing my source code and the second route displays this page you are looking at right now.</p>
 
-        <h2>Source Code:</h2>
         <main>
+          <div id="countdown">
+          </div>
           <pre>
             <code id="code" class="elixir">
             </code>
-          <pre>
+          </pre>
         </main>
 
         <script>
-          document.getElementById("code").append("something");
+          const request = (url, options) =>
+            fetch(url, options)
+              .then(checkStatus)
+              .then(parseJSON)
+              .then((data) => document.getElementById("code").textContent = data.code)
+              .catch((err) => { err });
+
+          const parseJSON = (response) => response.json()
+
+          const checkStatus = (response) => {
+            if (response.status >= 200 && response.status < 300) {
+              return response;
+            }
+
+           const error = new Error(response.statusText);
+           error.response = response;
+           throw error;
+          }
+
+          const counter = (n) => {
+            let counterDiv = document.getElementById("countdown");
+
+            if (n == 0) {
+              counterDiv.textContent = "";
+              request("http://localhost:8000/code");
+              return;
+            }
+
+            counterDiv.textContent = n;
+
+            setTimeout(() => {
+              counter(n-1);
+            }, 1000);
+          }
+
+          counter(3);
         </script>
       </body>
     </html>

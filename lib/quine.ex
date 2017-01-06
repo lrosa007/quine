92cc41e6c3a998ebbecfcbdf7dfbc5120400f4ef
lrosa007 <lucas@consys.io>
2017-01-06 09:50:26 -0500

    router and server responding to requests

--- lib/quine.ex
+++ lib/quine.ex
@@ -1,2 +1,28 @@
 defmodule Quine do
+  use Maru.Router
+
+  plug Plug.Parsers,
+    pass: ["*/*"],
+    json_decoder: Poison,
+    parsers: [:urlencoded, :json, :multipart]
+
+  get "/code" do
+    json conn, %{hello: :world}
+  end
+
+  get "/home" do
+    html conn, """
+    <html>
+      <body>
+        yo!!!
+      </body>
+    </html>
+    """
+  end
+
+  rescue_from :all do
+    conn
+    |> put_status(404)
+    |> text("Page Not Found")
+  end
 end
--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Quine.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :maru]]
   end
 
   defp deps do
